97b98e93c8f4fb45fb9e8e04364c9de043031a1a
Dan Chevalier <chevalier.dan@gmail.com>
2024-03-17 14:34:23 -0400

    Changing to Nuxt 3 notation for dynamic pages. Hooking in layout to the blog page as well

---
+++ pages/blog/[blog].vue
@@ -0,0 +1,11 @@
+<template>
+<NuxtLayout>
+  THE BLOG SLUG
+  <ContentDoc />
+</NuxtLayout>
+</template>
+<script setup>
+definePageMeta({
+  layout: 'blog'
+})
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Blog</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.slug">
+        <NuxtLink :to="`/blog/${post.slug}`">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useAsyncData } from 'nuxt/app'
+
+const { data: posts } = await useAsyncData('posts-list', () => queryContent('/blog').only(['title', 'slug']).find())
+</script>

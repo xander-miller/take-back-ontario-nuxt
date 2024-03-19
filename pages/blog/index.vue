57e64958ff3f43fe9e0239ea778ef10c8da6dade
Dan Chevalier <chevalier.dan@gmail.com>
2024-03-19 09:04:37 -0400

    Fix lints

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,7 +4,8 @@
       <h1>Blog</h1>
       <ul>
         <li v-for="post in posts" :key="post.slug">
-          <NuxtLink :to="`/blog/${post.slug}`"> {{ post.title }}
+          <NuxtLink :to="`/blog/${post.slug}`">
+            {{ post.title }}
           </NuxtLink>
         </li>
       </ul>

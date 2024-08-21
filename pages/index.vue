33393747419165f674bf80f9d97943d09845166c
Xander Miller <xander.miller@gmail.com>
2024-08-21 15:01:24 -0400

    Updates landing page
    - responsive design
    - content changes
    - join button goes to google form
    - log in nav hidden

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,14 @@
     <NuxtLayout name="bare">
       <div class="hero-container">
         <div class="red-triangle">
-          <div style="width: 100%; height: 100%;">
+          <div style="width: 100%; height: 100%">
             <svg
               viewBox="0 0 100 100"
               preserveAspectRatio="none"
             >
               <polygon
                 points="50,100 0,0 100,0"
-                style="fill: #b42f2b;"
+                style="fill: #b42f2b"
               />
             </svg>
           </div>
@@ -23,7 +23,7 @@
           >
             <polygon
               points="50,0 100,100 -25,150"
-              style="fill: rgba(0, 150, 227, 0.7);"
+              style="fill: rgba(0, 150, 227, 0.7)"
             />
           </svg>
         </div>
@@ -31,92 +31,105 @@
           src="/img/tbo-hero.png"
           class="tbo-hero"
         >
-        <nuxt-link
-          to="/join"
-        >
-          <button class="join-button hover:bg-red-800 bg-red-700 text-white font-bold py-2 px-4 rounded-full shadow">
-            Join
-            Take Back Ontario
+        <div class="simple-nav">
+          <NuxtLink
+            to="login"
+            class="front-button hidden"
+          >
+            Log In
+          </NuxtLink>
+        </div>
+        <nuxt-link to="https://docs.google.com/forms/d/e/1FAIpQLSc81gpDidaxKx8Mciv4Dr3oo0DjrdpNlCfcvGjDZp3mAlpW0g/viewform">
+          <button
+            class="join-button hover:bg-red-800 bg-red-700 mt-36 text-white font-bold py-2 px-4 rounded-full shadow"
+          >
+            Join Take Back Ontario
           </button>
         </nuxt-link>
       </div>
       <div />
-      <div class="simple-nav">
-        <NuxtLink
-          to="login"
-          class="front-button"
-        >
-          Log In
-        </NuxtLink>
-      </div>
       <div class="page-body-wrapper">
-        <div class="page-body prose-slate prose-2xl pb-36">
+        <div class="page-body prose-slate prose-base md:prose-2xl pb-36">
           <p class="mt-12 max-w-5xl ml-auto mr-auto">
             <span
-              class="mb-1 mt-10 text-left text-3xl font-bold max-w-5xl ml-0 mr-auto"
+              class="mb-1 mt-10 text-left text-xl md:text-3xl font-bold max-w-5xl ml-0 mr-auto"
             >
               Our province is broken...
             </span>
-            in an Ontario where people are struggling to find housing,
-            where the cost of living is skyrocketing, and people have little hope for the future.
-            Less than half of eligible voters are turning out to the polls,
-            and civic participation is extremely low.
-          </p>
-          <p class="mt-4 max-w-5xl ml-auto mr-auto">
-            <strong>Ontario citizens have been bullied and misled</strong> into not participating in the political process. We let the captured media and education system convince us that politics does not require our participation beyond voting for who they tell us to vote for and we stopped showing up.
-          </p>
-          <p class="mt-4 max-w-5xl ml-auto mr-auto">
-            Ontario citizens are frustrated but they are also disillusioned and disengaged.
-            <strong>Our leaders listen to those who can fund or fire them</strong>,
-            special interests no longer had any opposition and grew to fill the void we left behind.
-          </p>
-          <p class="max-w-4xl ml-0 mr-auto text-3xl font-medium text-left">
-            The result? A province that no longer works for Ontario citizens, but for corporations and activist ideologues.
+            the pandemic exposed the rot in public health and education. Then
+            came a cost of living crisis and a housing shortage. Ontario’s
+            children are pessimistic about their futures, and their parents’
+            trust in public institutions has plummeted. Apathy is a natural
+            response, but one we cannot afford. If we don’t show up for our
+            province, things are only going to get worse.
           </p>
           <h2
-            class="mb-1 mt-10 text-center text-5xl max-w-5xl ml-0 mr-auto"
-            style="color:#b42f2b;"
+            class="mb-1 mt-10 text-center text-2xl md:text-5xl max-w-5xl ml-0 mr-auto"
+            style="color: #b42f2b"
           >
             The world is run by those who show&nbsp;up
           </h2>
           <p class="mt-12 max-w-5xl ml-auto mr-auto">
-            Ontario citizens stopped participating. There were special interests who convinced use to stop participating but we let ourselves be convinced. <strong>The broken state of Ontario is our fault.</strong> We stopped showing up.
+            This didn’t happen by accident. Corrupt leadership, alongside our
+            media and education systems have convinced us that our political
+            power starts and ends with voting for pre-selected candidates once
+            every few years.
+            <strong>The broken state of Ontario is our fault.</strong> We
+            stopped showing up.
           </p>
           <p class="mt-4 max-w-5xl ml-auto mr-auto">
-            It’s not too late to take our province back. If Ontario citizens can take responsibility for breaking Ontario, then we can also take responsibility for fixing Ontario. <strong>Ontario has levers of power that Ontario citizens can operate. </strong>
-          </p><p class="mt-4 max-w-5xl ml-auto mr-auto">
-            We have neglected the levers of power of Ontario and they are now being operated by activist ideologues and special interests. These levers of power are the citizens to take back when we show up. 
+            It’s not too late to take our province back. Ontario citizens broke
+            Ontario by failing in our civic duty to engage in the operation of
+            Ontario. But we can fix Ontario by showing up and taking
+            responsibility for our province.
+            <strong>
+              Ontario has levers of power that Ontario citizens can operate.
+            </strong>
+          </p>
+          <p class="mt-4 max-w-5xl ml-auto mr-auto">
+            We have neglected the levers of power of Ontario and they are now
+            being operated by activist ideologues and special interests. These
+            levers of power are the citizens' to take back when we show up.
           </p>
           <h2
-            class="mb-1 mt-10 text-center text-5xl max-w-5xl ml-auto mr-auto"
-            style="color:#b42f2b;"
+            class="mb-1 mt-10 text-center text-2xl md:text-5xl max-w-5xl ml-auto mr-auto"
+            style="color: #b42f2b"
           >
-            So we show&nbsp;up
+            Therefore we show&nbsp;up
           </h2>
           <p class="mt-12 max-w-5xl ml-auto mr-auto">
-            It’s not too late to take our province back,
-            but it requires us to show up at the right place, the right time, and in the right way.
-            By joining Take Back Ontario, you will learn how to <strong>have an outsized impact</strong> by building your own political power,
-            identifying the levers of power, and pulling those levers in effective ways.
+            It is not too late to reverse this trend. We need to stand up to the
+            bullies and participate and learn how to engage in civics and
+            politics of Ontario. We need Ontario citizens to become political
+            organizers and civic leaders. Where we see problems we need to lean
+            in and take back instead of turning away. This requires us to
+            develop our political and civic skills so we show up at the right
+            place, the right time, and in the right way.
           </p>
           <h2
-            class="mb-1 mt-10 text-center text-5xl max-w-5xl ml-auto mr-auto"
-            style="color:#b42f2b;"
+            class="mb-1 mt-10 text-center text-2xl md:text-5xl max-w-5xl ml-auto mr-auto"
+            style="color: #b42f2b"
           >
             To take back Ontario
           </h2>
-          <p class="max-w-4xl text-center text-3xl font-medium mr-auto ml-auto">
-            You can make a big difference. You just have to show up.
+          <p class="mt-12 max-w-5xl ml-auto mr-auto z-10">
+            By showing up you can make an impact that far exceeds your single
+            vote at election time. Take Back Ontario is a network of citizens
+            who educate and encourage one another to show up, identify the
+            levers of power and to have an outside impact by taking them back
+            with integrity and responsibility. Want things to change? The world
+            is run by those who show up.
           </p>
-          <div class="text-center relative pb-24">
+          <div class="max-w-4xl text-center text-2xl md:text-3xl font-medium mr-auto ml-auto pb-24 z-10">
             I'm ready to show up!
             <nuxt-link
               class="text-center text-base hover:no-underline"
-              to="/join"
+              to="https://docs.google.com/forms/d/e/1FAIpQLSc81gpDidaxKx8Mciv4Dr3oo0DjrdpNlCfcvGjDZp3mAlpW0g/viewform"
             >
-              <button class="join-button hover:bg-red-800 bg-red-700 text-white font-bold py-2 px-4 rounded-full shadow !relative !block !transform-none !left-auto !bottom-auto mt-6 mr-auto ml-auto text">
-                Join
-                Take Back Ontario
+              <button
+                class="join-button hover:bg-red-800 bg-red-700 text-white font-bold py-2 px-4 rounded-full shadow !relative !block !transform-none !left-auto !bottom-auto mt-6 mr-auto ml-auto text"
+              >
+                Join Take Back Ontario
               </button>
             </nuxt-link>
           </div>
@@ -128,7 +141,7 @@
           >
             <polygon
               points="50,0 100,100 -25,150"
-              style="fill: rgba(0, 150, 227, 0.7);"
+              style="fill: rgba(0, 150, 227, 0.7)"
             />
           </svg>
         </div>
@@ -138,7 +151,7 @@
 </template>
 
 <script setup>
-import CnTowerChevron from '~/components/CnTowerChevron.vue';
+import CnTowerChevron from "~/components/CnTowerChevron.vue";
 </script>
 
 <style>
@@ -154,8 +167,8 @@ import CnTowerChevron from '~/components/CnTowerChevron.vue';
   position: absolute;
   bottom: 0;
   left: 50%;
-  transform: translateX(-50%);
-  z-index: 1000;
+  transform: translateX(-50%) translateY(50%);
+  z-index: 10;
 }
 
 .blue-triangle-separator {
@@ -166,7 +179,6 @@ import CnTowerChevron from '~/components/CnTowerChevron.vue';
   height: 15%;
   transform: skewX(45deg);
 }
-
 
 svg {
   position: absolute;
